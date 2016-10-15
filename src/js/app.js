30dd35761400b9fb645f83bb9780b2cfc516a797
Tyler Childs <tylerwchilds@gmail.com>
2016-10-15 11:25:00 -0700

    handle javaascript

---
+++ src/js/app.js
@@ -0,0 +1,37 @@
+(function(){
+  window.onload = function() {
+    document.addEventListener('change', handleSelection)
+  };
+
+  const handleSelection = function(){
+    const selects = document.querySelectorAll('select')
+    const values = getValues(selects);
+
+    selects.forEach(s => {
+      const options = s.querySelectorAll('option')
+
+      options.forEach((o) => markSelected(s, o, values))
+    })
+  }
+
+  const markSelected = function(s, o, values){
+    let notSelected = s.value !== o.value
+    notSelected = notSelected && values.indexOf(o.value) > -1
+
+    if(notSelected)
+      o.setAttribute('disabled', true);
+    else
+      o.removeAttribute('disabled');
+  }
+
+  const getValues = function($){
+    let values = [];
+
+    $.forEach(s => {
+      if(s.value !== "abstain")
+        values.push(s.value)
+    })
+
+    return values;
+  }
+})()
